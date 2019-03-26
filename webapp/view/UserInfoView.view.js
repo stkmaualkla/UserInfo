9ea5197669854719b9b2276eac93839a33d40ee9
Mauricio Alcala <mauricio.alcala@softtek.com>
2019-03-26 16:59:49 +0000

    other

--- webapp/view/UserInfoView.view.js
+++ webapp/view/UserInfoView.view.js
@@ -19,19 +19,19 @@ sap.ui.jsview("com.UserInfo.view.UserInfoView", {
 				
 					new sap.m.Label({wrapping: true,width: "100%",text: " TEST GETTING THE PERSONAL INFO FROM SAP.COM "}),
 					new sap.m.Label({wrapping: true,width: "20%",text: "JSON:"}), 
-					new sap.m.Input({value: " {/json}",width: "40%"}),
+					new sap.m.Input({value: " {/json}",width: "80%"}),
 					new sap.m.Label({wrapping: false,width: "20%",text: "STATUS: "}),
-					new sap.m.Input({value: " {/status}", width: "20%"}),
+					new sap.m.Input({value: " {/status}", width: "80%"}),
 					new sap.m.Label({wrapping: false,width: "20%",text: "Name: "}),
-					new sap.m.Input({value: " {/name}", width: "20%"}),
+					new sap.m.Input({value: " {/name}", width: "80%"}),
 					new sap.m.Label({wrapping: false,width: "20%",text: "Firstname: "}),
-					new sap.m.Input({value: " {/firstName}", width: "20%"}),
+					new sap.m.Input({value: " {/firstName}", width: "80%"}),
 					new sap.m.Label({wrapping: false,width: "20%",text: "Lastname: "}),
-					new sap.m.Input({ value: " {/lastName}", width: "20%"}),
+					new sap.m.Input({ value: " {/lastName}", width: "80%"}),
 					new sap.m.Label({wrapping: false,width: "20%",text: "Displayname: "}),
-					new sap.m.Input({value: " {/displayName}", width: "20%"}),
+					new sap.m.Input({value: " {/displayName}", width: "80%"}),
 					new sap.m.Label({wrapping: false,width: "20%",text: "Email: "}), 
-					new sap.m.Input({ value: " {/email}", width: "20%"})
+					new sap.m.Input({ value: " {/email}", width: "80%"})
 				
 				]
 		});
